8dc181971428202fb2e320194f95ee5e03d3a6a0
Shermin Sultana <rima64549@gmail.com>
2023-10-27 06:20:00 +0600

    add

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,16 @@
 import login from "../../assets/images/login/login.svg"
 
 const Login = () => {
+
+
+
+    const handleLogin = e =>{
+        e.preventDefault();
+
+        const email = e.target.email.value ;
+        const password = e.target.password.value ;
+        console.log(email, password)
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
@@ -9,25 +19,25 @@ const Login = () => {
            
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleLogin} className="card-body">
             <h1 className="text-xl font-bold">Login</h1>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
+                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+               <input type="submit" className="btn btn-primary" value='Login'/>
               </div>
             </form>
           </div>
